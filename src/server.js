502cc7dff31e7738b048fcc1fd91ba69cc9d90d7
kvmvkxnt <kamron1007dada@gmail.com>
2022-06-05 17:50:32 +0500

    Connected to main file

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+
+import checkToken from './middlewares/checkToken.js';
+
+import usersRouter from './routes/users.js';
+import messagesRouter from './routes/messages.js';
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('ok');
+app.use(express.json(), checkToken, usersRouter, messagesRouter);
+
+app.use((error, req, res, _) => {
+  if (error.status != 500) {
+    return res.status(error.status).json({
+      status: error.status,
+      message: error.message
+    })
+  }
+
+  fs.appendFileSync(path.join(process.cwd(), 'src', 'log.txt'),
+  `${req.url} ||| ${error.name} ||| ${Date.now()} ||| ${error.status} ||| ${error.message}\n`)
+
+  res.status(error.status).json({
+    status: error.status,
+    message: 'InternalServerError'
+  });
+
+  process.exit();
 });
 
 app.listen(PORT, () => {
--- src/utils/model.js
+++ src/utils/model.js
@@ -10,6 +10,6 @@ function write(fileName, data) {
   return true;
 }
 
-export default {
+export {
   read, write
 }
